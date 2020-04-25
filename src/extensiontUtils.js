eb7ded537fcd845b420c107cbc5ca944f77e7b99
David <david.susskind@walkme.com>
2020-04-25 22:30:14 +0300

    dynamically inject script (as opposed to content script) to get chrome permission to publish extension

--- src/extensiontUtils.js
+++ src/extensiontUtils.js
@@ -1,8 +1,21 @@
-function sendMessage(msg) {
+import { store } from "./store"
+import { INITIALIZE } from "./store/actionTypes"
+
+function sendMessage(msg, cb) {
   window.chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    window.chrome.tabs.sendMessage(tabs[0].id, msg)
+    window.chrome.tabs.sendMessage(tabs[0].id, msg, cb)
   })
 }
 
+function getInitialStateFromInsideExtension() {
+  const file = "runInPage.js"
+  chrome.tabs.executeScript({ file }, () =>
+    sendMessage("getInitialState", response => {
+      if (response) {
+        store.dispatch({ type: INITIALIZE, payload: response })
+      }
+    })
+  )
+}
 const isExtension = () => window?.chrome?.tabs
-export { sendMessage, isExtension }
+export { getInitialStateFromInsideExtension, sendMessage, isExtension }
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import { store } from "./store"
 import { loadWalkMe } from "./core"
-import { INITIALIZE } from "./store/actionTypes"
+import { getInitialStateFromInsideExtension } from "./extensiontUtils"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 if (window?.chrome?.tabs) {
-  window.chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    window.chrome.tabs.sendMessage(tabs[0].id, "getInitialState", response => {
-      if (response) {
-        store.dispatch({ type: INITIALIZE, payload: response })
-      }
-    })
-  })
+  getInitialStateFromInsideExtension()
 } else {
   loadWalkMe()
 }
